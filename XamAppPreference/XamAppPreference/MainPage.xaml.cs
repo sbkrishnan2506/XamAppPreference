c64beee5184b200b66bfd024901ee5cf0c311b32
Bala Krish <56611791+sbkrishnan2506@users.noreply.github.com>
2023-05-30 16:03:08 -0500

    Add files via upload
    Initial commit

---
+++ XamAppPreference/XamAppPreference/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamAppPreference
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            App.Current.Properties.Add("first_name", "John");
+            App.Current.SavePropertiesAsync();
+
+            GetPreference();
+        }
+
+        private void GetPreference()
+        {
+            var val = App.Current.Properties["first_name"]; 
+
+        }
+    }
+}
